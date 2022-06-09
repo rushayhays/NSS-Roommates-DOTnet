b1cf2ee8b4ae5e9c8e4b42a65dea800f59b9f32a
rushayhays <rushayhays@outlook.com>
2022-06-09 15:36:21 -0500

    first commit

---
+++ Roommates/Repositories/ChoreRepository.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+
+namespace Roommates.Repositories
+{
+    /// <summary>
+    ///  This class is responsible for interacting with Chore data.
+    ///  It inherits from the BaseRepository class so that it can use the BaseRepository's Connection property
+    /// </summary>
+    public class ChoreRepository : BaseRepository
+    {
+        /// <summary>
+        ///  When new RoomRepository is instantiated, pass the connection string along to the BaseRepository
+        /// </summary>
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+       
+        
+        
+        
+        public List<Chore> GetAll()
+        {
+            //  We must "use" the database connection.
+            //  Because a database is a shared resource (other applications may be using it too) we must
+            //  be careful about how we interact with it. Specifically, we Open() connections when we need to
+            //  interact with the database and we Close() them when we're finished.
+            //  In C#, a "using" block ensures we correctly disconnect from a resource even if there is an error.
+            //  For database connections, this means the connection will be properly closed.
+            using (SqlConnection conn = Connection)
+            {
+
+                conn.Open();
+
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = "SELECT Id, Name FROM Chore";
+
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        List<Chore> chores = new List<Chore>();
+
+
+                        while (reader.Read())
+                        {
+
+
+                            int idColumnPosition = reader.GetOrdinal("Id");
+
+
+                            int idValue = reader.GetInt32(idColumnPosition);
+
+                            int nameColumnPosition = reader.GetOrdinal("Name");
+                            string nameValue = reader.GetString(nameColumnPosition);
+
+
+
+
+
+                            Chore chore = new Chore
+                            {
+                                Id = idValue,
+                                Name = nameValue,
+                            };
+
+
+
+                            chores.Add(chore);
+                        }
+
+                        return chores;
+                    }
+                }
+
+            }
+        }
+        public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name From Chore WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Chore chore = null;
+
+                        if(reader.Read())
+                        {
+                            chore = new Chore
+                            {
+                                Id = id,
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+                        }
+                        return chore;
+                    }
+                }
+            }
+        }
+
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Chore (Name)
+                                            OUTPUT INSERTED.Id
+                                            VALUES(@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                }
+            }
+        }
+
+    }
+}
