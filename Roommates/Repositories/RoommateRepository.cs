b70709e81b50be99f590e317a525cc8f856f5e51
rushayhays <rushayhays@outlook.com>
2022-06-09 16:01:34 -0500

    working on roommate repository methog GetById

---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Roommate.FirstName, Roommate.LastName, Roommate.RentPortion, Roommate.MoveInDate, Roommate.RoomId, Room.Id 
+                                        FROM Roommate
+                                        LEFT JOIN Room on Roommate.RoomId = Room.Id
+                                        WHERE Roommate.Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate roommate = null;
+                        if (reader.Read())
+                        {
+                            roommate = new Roommate
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                Room =
+                                {
+                                    Id= reader.GetString(reader.GetOrdinal(Id)),
+                                    Name = reader.GetString(reader.GetOrdinal("Name"))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
